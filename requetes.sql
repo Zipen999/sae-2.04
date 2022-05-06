cd6da92e7a65bac4e34a73527145de357cf0bfb5
masson63 <33490275+masson63@users.noreply.github.com>
2022-05-06 08:51:06 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -4,9 +4,15 @@ WHERE v.vues > 10000000
 	AND c.idChaine = p.chaine
 	AND p.video = v.idVideo;
 
+
 SELECT v.dislikes,c.nom
 FROM Video v,Publier p,Chaine c
 WHERE c.idChaine = p.chaine
 	AND p.video = v.idVideo
 	AND v.dislikes >= ALL(SELECT v2.dislikes
 						FROM Video v2);
+
+SELECT v.vues
+FROM Video v
+WHERE v.dateSortie <= '17-14-11'
+	AND v.dateSortie >= '17-10-11';
