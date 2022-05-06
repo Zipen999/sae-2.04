52ce392883757b5340a4e4c1a7dd3ff27c03c5da
masson63 <33490275+masson63@users.noreply.github.com>
2022-05-06 08:30:16 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -1,1 +1,10 @@
+SELECT c.nom
+FROM Chaine c, Video v,Publier p
+WHERE v.vues > 10000000
+	AND c.idChaine = p.chaine
+	AND p.video = v.idVideo;
 
+
+SELECT MAX(v.dislikes),v.titre,
+FROM Video v
+WHERE v.dateSortie = CURRENT_DATE-5*365;
