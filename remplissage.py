785b1b51e83be6e3a2b27d696e22d3e1c3d34e1d
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2022-05-11 11:55:58 +0200

    insertion des categories

--- remplissage.py
+++ remplissage.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import psycopg2 as psy
+import json
 
 from getpass import getpass
 
@@ -7,6 +8,15 @@ def Table(user, userdb, pwd):
 	data=pd.read_csv(r'TrendingFR.csv')
 	df=pd.DataFrame(data)
 	df = df.drop_duplicates(subset=['video_id' , 'trending_date']) # Supprime les lignes dupliquées (Vidéos passés en tendances le même jour, impossible à traiter / incohérent sur youtube car une video peut passer en tendance une fois par jour)
+	################### TRAITEMENT DES CATEGORIES
+	id_vers_categorie = {}
+	with open('FR_category_id.json', 'r') as f:
+	    data = json.load(f)
+	    for cat in data['items']:
+	        id_vers_categorie[cat['id']] = cat['snippet']['title']
+	df['categoryId'] = df['categoryId'].astype(str)
+	df.insert(6,'category',df['categoryId'].map(id_vers_categorie))
+
 	co = None
 	try:
 		co=psy.connect(host='berlin',database=userdb,user=user,password=pwd)
@@ -16,19 +26,28 @@ def Table(user, userdb, pwd):
 		print("Suppression des tables déjà existantes...")
 		curs.execute('''DROP TABLE IF EXISTS Publier;
 						DROP TABLE IF EXISTS Chaine;
-						DROP TABLE IF EXISTS Video;''')
+						DROP TABLE IF EXISTS Video;
+						DROP TABLE IF EXISTS Category;''')
 	####################### Creation tables
 		print("Création des tables...")
-		curs.execute('''CREATE TABLE Video(
-								idVideo char(11),
-								dateTrending date,
-								titre varchar(300),
-								dateSortie timestamp,
-								vues numeric,
-								likes numeric,
-								dislikes numeric,
-								commentaires numeric,
-								CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTrending)
+		curs.execute('''
+						CREATE TABLE Categorie(
+							idCategorie	numeric,
+							nom varchar(30),
+							CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie)
+						);
+						CREATE TABLE Video(
+							idVideo char(11),
+							dateTendance date,
+							titre varchar(300),
+							dateSortie timestamp,
+							vues numeric,
+							likes numeric,
+							dislikes numeric,
+							commentaires numeric,
+							categorie numeric,
+							CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTendance),
+							CONSTRAINT fk_Categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
 						);
 						CREATE TABLE Chaine(
 							idChaine 	char(24),
@@ -37,12 +56,13 @@ def Table(user, userdb, pwd):
 						);
 						CREATE TABLE Publier(
 							video char(11),
-							dateTrending date,
+							dateTendance date,
 							chaine char(24),
-							CONSTRAINT pk_publier PRIMARY KEY (video,dateTrending,chaine),
+							CONSTRAINT pk_publier PRIMARY KEY (video,dateTendance,chaine),
 							CONSTRAINT fk_chaine FOREIGN KEY (chaine) REFERENCES Chaine(idChaine),
-							CONSTRAINT fk_video FOREIGN KEY (video, dateTrending) REFERENCES Video(idVideo, dateTrending)
-						);''')
+							CONSTRAINT fk_video FOREIGN KEY (video, dateTendance) REFERENCES Video(idVideo, dateTendance)
+						);
+						''')
 
 
 	####################### Insertion de données
@@ -59,6 +79,10 @@ def Table(user, userdb, pwd):
 
 				curs.execute('''INSERT INTO Publier VALUES (%s,%s,%s);''',
 					(row.video_id,row.trending_date,row.channelId))
+
+				curs.execute('''INSERT INTO Categorie VALUES (%s,%s) ON CONFLICT ON CONSTRAINT pk_Categorie DO NOTHING;''',
+					(row.categoryId,row.category))
+
 	####################### Fin insertion
 		co.commit()
 		curs.close()
