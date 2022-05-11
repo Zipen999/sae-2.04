743f825275708357a1f223a47ee4a1cd1b0ff499
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2022-05-11 11:50:31 +0200

    Update requetes.sql

--- requetes.sql
+++ requetes.sql
@@ -27,16 +27,23 @@ ORDER BY prcnt;
 
 SELECT c.nom, count(*) AS NbVids
 FROM Video v
-	INNER JOIN Publier p ON v.idVideo = p.Video
-	AND v.dateTendance = p.dateTendance
-	INNER JOIN Chaine c ON c.idChaine = p.Chaine
-	GROUP BY c.nom
-	HAVING count(*)>=200
-	ORDER BY NbVids DESC
-	LIMIT 15;
+INNER JOIN Publier p ON v.idVideo = p.Video
+AND v.dateTendance = p.dateTendance
+INNER JOIN Chaine c ON c.idChaine = p.Chaine
+GROUP BY c.nom
+HAVING count(*)>=200
+ORDER BY NbVids DESC
+LIMIT 15;
 
 
 
 -- Prcnt de likes et dislikes par rapport aux vues
 SELECT titre, (likes+dislikes)/vues *100 AS prcnt FROM Video
 	WHERE vues != 0 ORDER BY prcnt DESC;
+
+-- Les catégories les plus populaires
+SELECT ca.nom , count(*) AS NbVids
+FROM Video v
+INNER JOIN Categorie ca ON ca.idCategorie = v.categorie
+GROUP BY ca.idCategorie
+ORDER BY NbVids DESC;
