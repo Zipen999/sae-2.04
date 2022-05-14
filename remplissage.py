b8248c19b46e72c61d6917da809dcb03fb449b58
CPdA-Omi <92311246+CPdA-Omi@users.noreply.github.com>
2022-05-14 11:45:13 +0200

    Update remplissage.py

--- remplissage.py
+++ remplissage.py
@@ -23,52 +23,52 @@ def Table(user, userdb, pwd):
 
 		curs = co.cursor()
 	#######################
-		print("Suppression des tables déjà existantes...")
-		curs.execute('''DROP TABLE IF EXISTS Publier;
-						DROP TABLE IF EXISTS Chaine;
-						DROP TABLE IF EXISTS Video;
-						DROP TABLE IF EXISTS Category;''')
+		print("\nTemps estimé de refonte de la base de données : 8min 30s")
+		var=input("Refaire la base de données (O/n) ? ")
+		if(var=='O' or var=='o' or var==1):
+			print("Suppression des tables déjà existantes...")
+			curs.execute('''DROP TABLE IF EXISTS Publier;
+							DROP TABLE IF EXISTS Chaine;
+							DROP TABLE IF EXISTS Video;
+							DROP TABLE IF EXISTS Category;''')
 	####################### Creation tables
-		print("Création des tables...")
-		curs.execute('''
-						CREATE TABLE Categorie(
-							idCategorie	numeric,
-							nom varchar(30),
-							CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie)
-						);
-						CREATE TABLE Video(
-							idVideo char(11),
-							dateTendance date,
-							titre varchar(300),
-							dateSortie timestamp,
-							vues numeric,
-							likes numeric,
-							dislikes numeric,
-							commentaires numeric,
-							categorie numeric,
-							CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTendance),
-							CONSTRAINT fk_Categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
-						);
-						CREATE TABLE Chaine(
-							idChaine 	char(24),
-							nom			varchar(100),
-							CONSTRAINT pk_Chaine PRIMARY KEY (idChaine)
-						);
-						CREATE TABLE Publier(
-							video char(11),
-							dateTendance date,
-							chaine char(24),
-							CONSTRAINT pk_publier PRIMARY KEY (video,dateTendance,chaine),
-							CONSTRAINT fk_chaine FOREIGN KEY (chaine) REFERENCES Chaine(idChaine),
-							CONSTRAINT fk_video FOREIGN KEY (video, dateTendance) REFERENCES Video(idVideo, dateTendance)
-						);
-						''')
+			print("Création des tables...")
+			curs.execute('''
+							CREATE TABLE Categorie(
+								idCategorie	numeric,
+								nom varchar(30),
+								CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie)
+							);
+							CREATE TABLE Video(
+								idVideo char(11),
+								dateTendance date,
+								titre varchar(300),
+								dateSortie timestamp,
+								vues numeric,
+								likes numeric,
+								dislikes numeric,
+								commentaires numeric,
+								categorie numeric,
+								CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTendance),
+								CONSTRAINT fk_Categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
+							);
+							CREATE TABLE Chaine(
+								idChaine 	char(24),
+								nom			varchar(100),
+								CONSTRAINT pk_Chaine PRIMARY KEY (idChaine)
+							);
+							CREATE TABLE Publier(
+								video char(11),
+								dateTendance date,
+								chaine char(24),
+								CONSTRAINT pk_publier PRIMARY KEY (video,dateTendance,chaine),
+								CONSTRAINT fk_chaine FOREIGN KEY (chaine) REFERENCES Chaine(idChaine),
+								CONSTRAINT fk_video FOREIGN KEY (video, dateTendance) REFERENCES Video(idVideo, dateTendance)
+							);
+							''')
 
 
 	####################### Insertion de données
-		print("\nTemps estimé d'insertion des données : 6min")
-		var=input("Insérer les données (O/n) ? ")
-		if(var=='O' or var=='o' or var==1):
 			print("Insertion des données...")
 			for row in df.itertuples():
 				curs.execute('''INSERT INTO Video VALUES (%s,%s,%s,%s,%s,%s,%s,%s);''',
