58a4312e1bc9b9fa085d7c2470378e0dcc2de5bf
CPdA-Omi <92311246+CPdA-Omi@users.noreply.github.com>
2022-05-16 13:21:53 +0200

    Update remplissage.py

--- remplissage.py
+++ remplissage.py
@@ -8,6 +8,7 @@ def Table(user, userdb, pwd):
 	data=pd.read_csv(r'TrendingFR.csv')
 	df=pd.DataFrame(data)
 	df = df.drop_duplicates(subset=['video_id' , 'trending_date']) # Supprime les lignes dupliquées (Vidéos passés en tendances le même jour, impossible à traiter / incohérent sur youtube car une video peut passer en tendance une fois par jour)
+	
 	################### TRAITEMENT DES CATEGORIES
 	id_vers_categorie = {}
 	with open('FR_category_id.json', 'r') as f:
@@ -19,62 +20,64 @@ def Table(user, userdb, pwd):
 	# print(id_vers_categorie)
 	# print(df['category'].unique())
 	####################### Fin du traitement
+
 	co = None
 	try:
 		co=psy.connect(host='berlin',database=userdb,user=user,password=pwd)
 
 		curs = co.cursor()
+	#######################
+		print("\nTemps estimé de refonte de la base de données : 8min 30s")
+		var=input("Refaire la base de données (O/n) ? ")
+		if(var=='O' or var=='o' or var==1):
 	####################### Suppression des tables
-		print("Suppression des tables déjà existantes...")
-		curs.execute('''DROP TABLE IF EXISTS Publier;
-						DROP TABLE IF EXISTS Chaine;
-						DROP TABLE IF EXISTS Video;
-						DROP TABLE IF EXISTS Category;''')
+			print("Suppression des tables déjà existantes...")
+			curs.execute('''DROP TABLE IF EXISTS Publier;
+							DROP TABLE IF EXISTS Chaine;
+							DROP TABLE IF EXISTS Video;
+							DROP TABLE IF EXISTS Category;''')
 	####################### Creation tables
-		print("Création des tables...")
-		curs.execute('''
-						CREATE TABLE Categorie(
-							idCategorie	numeric,
-							nom varchar(30),
-							CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie)
-						);
-						CREATE TABLE Video(
-							idVideo char(11),
-							dateTendance date,
-							titre varchar(300),
-							dateSortie timestamp,
-							vues numeric,
-							likes numeric,
-							dislikes numeric,
-							commentaires numeric,
-							categorie numeric,
-							CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTendance),
-							CONSTRAINT fk_Categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
-						);
-						CREATE TABLE Chaine(
-							idChaine 	char(24),
-							nom			varchar(100),
-							CONSTRAINT pk_Chaine PRIMARY KEY (idChaine)
-						);
-						CREATE TABLE Publier(
-							video char(11),
-							dateTendance date,
-							chaine char(24),
-							CONSTRAINT pk_publier PRIMARY KEY (video,dateTendance,chaine),
-							CONSTRAINT fk_chaine FOREIGN KEY (chaine) REFERENCES Chaine(idChaine),
-							CONSTRAINT fk_video FOREIGN KEY (video, dateTendance) REFERENCES Video(idVideo, dateTendance)
-						);
-						''')
+			print("Création des tables...")
+			curs.execute('''
+							CREATE TABLE Categorie(
+								idCategorie	numeric,
+								nom varchar(30),
+								CONSTRAINT pk_Categorie PRIMARY KEY (idCategorie)
+							);
+							CREATE TABLE Video(
+								idVideo char(11),
+								dateTendance date,
+								titre varchar(300),
+								dateSortie timestamp,
+								vues numeric,
+								likes numeric,
+								dislikes numeric,
+								commentaires numeric,
+								categorie numeric,
+								CONSTRAINT pk_Video PRIMARY KEY (idVideo, dateTendance),
+								CONSTRAINT fk_Categorie FOREIGN KEY (categorie) REFERENCES Categorie(idCategorie)
+							);
+							CREATE TABLE Chaine(
+								idChaine 	char(24),
+								nom			varchar(100),
+								CONSTRAINT pk_Chaine PRIMARY KEY (idChaine)
+							);
+							CREATE TABLE Publier(
+								video char(11),
+								dateTendance date,
+								chaine char(24),
+								CONSTRAINT pk_publier PRIMARY KEY (video,dateTendance,chaine),
+								CONSTRAINT fk_chaine FOREIGN KEY (chaine) REFERENCES Chaine(idChaine),
+								CONSTRAINT fk_video FOREIGN KEY (video, dateTendance) REFERENCES Video(idVideo, dateTendance)
+							);
+							''')
 
 
 	####################### Insertion de données
-		print("Temps estimé d'insertion des données : 5min")
-		var=input("Insérer les données (O/n) ? ")
-		if(var=='o' or var=='O' or var==1):
 			print("Insertion des données...")
 			for row in df.itertuples():
-				curs.execute('''INSERT INTO Video VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);''',
-					(row.video_id,row.trending_date,row.title,row.publishedAt,row.view_count,row.likes,row.dislikes,row.comment_count,row.category_id))
+				curs.execute('''INSERT INTO Video VALUES (%s,%s,%s,%s,%s,%s,%s,%s);''',
+					(row.video_id,row.trending_date,row.title,row.publishedAt,row.view_count,row.likes,row.dislikes,row.comment_count))
 
 				curs.execute('''INSERT INTO Chaine VALUES (%s,%s) ON CONFLICT ON CONSTRAINT pk_Chaine DO NOTHING;''',
 					(row.channelId,row.channelTitle))
@@ -88,14 +91,17 @@ def Table(user, userdb, pwd):
 	####################### Fin insertion
 		co.commit()
 		curs.close()
+		print("Done")
 	
 	except(Exception,psy.DatabaseError) as error:
+	####################### Erreur du try
 		print(error)
 	finally:
+	####################### Après le try
 		if co is not None:
 			co.close()
 
-user=input("Nom d'utilisateur : ")
+user=input("Nom d'utilisateur et de la base de donnée : ")
 pwd = getpass(prompt="Mot de passe : ")
 userdb = "db" + user 
 Table(user, userdb, pwd)
