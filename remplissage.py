c60f91229caeb47b43647c5b59a8d781805a3403
Zipen999 <78689752+Zipen999@users.noreply.github.com>
2022-05-18 20:24:10 +0200

    optimisation du code, de 8:50 d'insertion a 2:40 !

--- remplissage.py
+++ remplissage.py
@@ -17,8 +17,6 @@ def Table(user, userdb, pwd):
 	        id_vers_categorie[cat['id']] = cat['snippet']['title']
 	df['categoryId'] = df['categoryId'].astype(str)
 	df.insert(6,'category',df['categoryId'].map(id_vers_categorie))
-	# print(id_vers_categorie)
-	# print(df['category'].unique())
 	####################### Fin du traitement
 
 	co = None
@@ -29,16 +27,35 @@ def Table(user, userdb, pwd):
 	#######################
 		print("\nTemps estimé de refonte de la base de données : 8min 30s")
 		var=input("Refaire la base de données (O/n) ? ")
-		if(var=='O' or var=='o' or var=='1' or var=="oui" or var=="Oui"):
+		if(var=='O' or var=='o' or var==1):
 	####################### Suppression des tables
 			print("Suppression des tables déjà existantes...")
-			curs.execute('''DROP TABLE IF EXISTS Publier;
+			curs.execute('''DROP TABLE IF EXISTS CSV;
+							DROP TABLE IF EXISTS Publier;
 							DROP TABLE IF EXISTS Chaine;
 							DROP TABLE IF EXISTS Video;
 							DROP TABLE IF EXISTS Categorie;''')
 	####################### Creation tables
 			print("Création des tables...")
 			curs.execute('''
+
+							CREATE TABLE CSV(
+								video_id char(11),
+								title varchar(300),
+								publishedAt timestamp,
+								channelId	char(24),
+								channelTitle varchar(100),
+								categoryId	numeric,
+								category 	varchar(100),
+								trending_date date,
+								view_count numeric,
+								likes numeric,
+								dislikes numeric,
+								comment_count numeric,
+								CONSTRAINT pk_CSV PRIMARY KEY (video_id, trending_date)
+
+							);
+
 							CREATE TABLE Categorie(
 								idCategorie	numeric,
 								nom varchar(30),
@@ -76,18 +93,24 @@ def Table(user, userdb, pwd):
 	####################### Insertion de données
 			print("Insertion des données...")
 			for row in df.itertuples():
-				curs.execute('''INSERT INTO Video VALUES (%s,%s,%s,%s,%s,%s,%s,%s);''',
-					(row.video_id,row.trending_date,row.title,row.publishedAt,row.view_count,row.likes,row.dislikes,row.comment_count))
+				curs.execute('''INSERT INTO CSV VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);''',
+					(row.video_id,row.title,row.publishedAt,row.channelId,row.channelTitle,
+						row.categoryId,row.category,row.trending_date,row.view_count,row.likes,row.dislikes,row.comment_count))
 
-				curs.execute('''INSERT INTO Chaine VALUES (%s,%s) ON CONFLICT ON CONSTRAINT pk_Chaine DO NOTHING;''',
-					(row.channelId,row.channelTitle))
+			curs.execute(''' INSERT INTO Categorie SELECT DISTINCT categoryId, category FROM CSV;''')
+			curs.execute(''' INSERT INTO Video
+							SELECT video_id,trending_date,title,publishedAt,view_count,likes, dislikes, comment_count, categoryId FROM CSV;''')
 
-				curs.execute('''INSERT INTO Publier VALUES (%s,%s,%s);''',
-					(row.video_id,row.trending_date,row.channelId))
+			curs.execute(''' INSERT INTO Chaine(idChaine, nom)
+							SELECT DISTINCT channelId,channelTitle 
+							FROM CSV
+							ON CONFLICT ON CONSTRAINT pk_Chaine DO NOTHING;''')
 
-				curs.execute('''INSERT INTO Categorie VALUES (%s,%s) ON CONFLICT ON CONSTRAINT pk_Categorie DO NOTHING;''',
-					(row.categoryId,row.category))
 
+			curs.execute(''' INSERT INTO Publier
+							SELECT video_id, trending_date , channelId FROM CSV;''')
+
+            		curs.execute('''DROP TABLE IF EXISTS CSV;''')
 	####################### Fin insertion
 		co.commit()
 		curs.close()
@@ -101,7 +124,7 @@ def Table(user, userdb, pwd):
 		if co is not None:
 			co.close()
 
-user=input("Nom d'utilisateur (et de la base de donnée) : ")
+user=input("Nom d'utilisateur et de la base de donnée : ")
 pwd = getpass(prompt="Mot de passe : ")
 userdb = "db" + user 
 Table(user, userdb, pwd)
